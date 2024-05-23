a43d7586e594060c7456b654ad7f166a106440d3
cameron-leung <cameronleung@berkeley.edu>
2024-05-23 15:32:45 -0700

    Add files via upload

---
+++ bare_shop/src/test/java/baremagazine/shop/ShopApplicationTests.java
@@ -0,0 +1,13 @@
+package baremagazine.shop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
